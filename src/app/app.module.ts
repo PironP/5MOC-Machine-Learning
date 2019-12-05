f160f0f1abcdc6faec3c578ce70a3711aebd9b5c
pironp <piron.pierre@gmail.com>
2019-12-05 12:41:37 +0100

    Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ChifoumiComponent } from './chifoumi/chifoumi.component';
+import { HomepageComponent } from './homepage/homepage.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomepageComponent,
+  },
+  {
+      path: 'game',
+      component: ChifoumiComponent,
+  },
+  {
+      path: 'search',
+      component: ChifoumiComponent, // TO UPDATE
+  },
+  {
+      path: 'help',
+      component: ChifoumiComponent, // TO UPDATE
+  },
+  {
+    path: '',
+    redirectTo: 'home', pathMatch: 'full'
+  },
+  {
+      path: '**',
+      redirectTo: '/home',
+  },
+];
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ChifoumiComponent,
+    HomepageComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [RouterModule]
 })
 export class AppModule { }
